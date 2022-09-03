7ced9157bb30851bbb587091618451fa6717229c
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-03 12:40:22 -0500

    added user model, built out match model more

--- CodTrackerCore/Models/Match.cs
+++ CodTrackerCore/Models/Match.cs
@@ -5,5 +5,16 @@
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        public User User { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Kills { get; set; }
+
+        public int Deaths { get; set; }
+
+        public int Damage { get; set; }
+        
     }
 }
---
+++ CodTrackerCore/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CodTrackerCore.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
