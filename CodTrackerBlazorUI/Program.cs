f0f9050cb376866c06e356add3f9f9a25677a82b
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-03 12:16:01 -0500

    initial commit

---
+++ CodTrackerBlazorUI/Program.cs
@@ -0,0 +1,28 @@
+using CodTrackerBlazorUI.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment() is false)
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
