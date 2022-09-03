4b196522f52830227612bacce656cec24b8dda9e
StevanFreeborn <sfreeborn7@gmail.com>
2022-09-03 12:31:17 -0500

    added mongo.bson library to core project, begun working through core models

---
+++ CodTrackerCore/Models/Match.cs
@@ -0,0 +1,9 @@
+﻿namespace CodTrackerCore.Models
+{
+    public class Match
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
